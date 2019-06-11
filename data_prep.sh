67e279f747cc2091d397a10c8ffc01099842c594
Fei Wu <fwu@c03.clsp.jhu.edu>
2019-06-11 14:38:06 -0400

    convert to PCM

--- data_prep.sh
+++ data_prep.sh
@@ -10,7 +10,18 @@ rm -f $data/utt2spk
 for wav in $corpus/*; do
     uttID=$(basename $wav)
     uttID=${uttID%".wav"}
-    echo "$uttID $wav" >> $data/wav.scp 
+
+    format=$(file $wav | cut -d, -f 3 | cut -d ' ' -f 3)
+    echo "$uttID.wav : $format"
+    if [ "$format" == "PCM" ]; then
+        echo "$uttID $wav" >> $data/wav.scp
+    else
+        echo "$uttID ffmpeg -v 8 -i $wav -f wav -acodec pcm_s16le -|" >> $data/wav.scp
+    fi 
     echo "$uttID 2" >> $data/reco2num_spk
     echo "$uttID $uttID" >> $data/utt2spk 
 done
+
+./utils/utt2spk_to_spk2utt.pl $data/utt2spk > $data/spk2utt
+./utils/validate_data_dir.sh --no-feats --no-text $data 
+./utils/fix_data_dir.sh $data
