5f1d629286bb26f4cfa4dc33249348aa5412f5fc
Fei Wu <fwu@c03.clsp.jhu.edu>
2019-06-11 14:53:27 -0400

    Check exist files in data

--- file_check.sh
+++ file_check.sh
@@ -6,9 +6,7 @@ mkdir -p $1/.backup
 for file in $1/*; do 
     if [ -f $file ]; then
         echo "$file exists. Moved old file to .backup"
-        cp $file $1/.backup/$(basename $file)
-        rm $file
-        touch $file
+        mv $file $1/.backup/$(basename $file)
     fi
 done
 
